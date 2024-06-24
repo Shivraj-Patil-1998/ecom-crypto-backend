bc83142cee36ae254f55f4d1d83e13f4d2c48fd1
shivraj <shivraj@simplileap.com>
2024-06-24 15:44:20 +0530

    last commit

--- controller/createTransactions.js
+++ controller/createTransactions.js
@@ -1,13 +1,13 @@
-const { Op } = require("sequelize");
+require("dotenv").config();
 const models = require("../models/index");
 const { Transactions } = models;
-const { v4: uuidv4 } = require('uuid');
+const { Op } = require("sequelize");
 
 async function createTransactions(req, res) {
   try {
-    // Destructure data from req.body
-    let {
+    const {
       assetId,
+      transactionId,
       transactiontype,
       toAddress,
       exactAmount,
@@ -19,16 +19,10 @@ async function createTransactions(req, res) {
       status,
     } = req.body;
 
-    // Generate a UUID for transactionId
-    const transactionId = uuidv4();
-
-    // Convert toAddress to lowercase
-    toAddress = toAddress.toLowerCase();
-
-    // Define a time threshold (10 minutes ago)
+   
     const tenMinutesAgo = new Date(Date.now() - 10 * 60 * 1000);
 
-    // Query existing transactions within the last 10 minutes for the same criteria
+    
     const existingTransactions = await Transactions.findAll({
       where: {
         [Op.and]: [
@@ -55,15 +49,14 @@ async function createTransactions(req, res) {
       },
     });
 
-    // Check if there are existing transactions
     if (existingTransactions.length > 0) {
-      // Check if any existing transaction allows the new one based on assetId
+     
       const isAllowed = existingTransactions.some(
         (transaction) => transaction.assetId !== assetId
       );
 
-      // If not allowed, return 403 Forbidden status
       if (!isAllowed) {
+        
         return res.status(403).json({
           success: false,
           message: "Transaction creation blocked. Please wait for 10 minutes.",
@@ -71,12 +64,12 @@ async function createTransactions(req, res) {
       }
     }
 
-    // Create the new transaction in the database
+    
     const createTransaction = await Transactions.create({
       assetId,
       transactionId,
       transactiontype,
-      toAddress, // This will be the lowercase version
+      toAddress,
       exactAmount,
       amount,
       orderId,
@@ -86,14 +79,12 @@ async function createTransactions(req, res) {
       status,
     });
 
-    // Return success response
     res.json({
       success: true,
       message: "Transaction Created Successfully",
       body: { createTransaction },
     });
   } catch (error) {
-    // Handle any errors that occur during transaction creation
     console.error("Failed to create transaction", error);
     res.status(500).json({
       success: false,
