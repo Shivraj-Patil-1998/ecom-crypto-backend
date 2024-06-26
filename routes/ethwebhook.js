de8f1d2f977c09c9dc2434506b0730d3fa7a51d7
shivraj <shivraj@simplileap.com>
2024-06-26 15:10:16 +0530

    last commit

--- routes/ethwebhook.js
+++ routes/ethwebhook.js
@@ -2,6 +2,10 @@ const express = require('express');
 const { ethWebhook } = require('../webhook/eth');
 const { usdcWebhook } = require('../webhook/usdc');
 const { usdtWebhook } = require('../webhook/usdt');
+const { bscusdcWebhook } = require('../webhook/bscusdc');
+const { bscusdtWebhook } = require('../webhook/bscusdt');
+const { polygonusdcWebhook } = require('../webhook/polygonusdc');
+const { polygonusdtWebhook } = require('../webhook/polygonusdt');
 
 const router = express.Router();
 
@@ -11,10 +15,10 @@ router.post('/usdc', usdcWebhook);
 router.post('/usdt', usdtWebhook);
 // router.post('/usdctron', ethWebhook);
 // router.post('/usdttron', ethWebhook);
-// router.post('/usdcpolygon', ethWebhook);
-// router.post('/usdtpolygon', ethWebhook);
-// router.post('/usdcbsc', ethWebhook);
-// router.post('/usdtbsc', ethWebhook);
+router.post('/usdcpolygon', polygonusdcWebhook);
+router.post('/usdtpolygon', polygonusdtWebhook);
+router.post('/usdcbsc', bscusdcWebhook);
+router.post('/usdtbsc', bscusdtWebhook);
 // router.post('/eth', ethWebhook);
 
 module.exports = router;
---
+++ webhook/bscusdc.js
@@ -0,0 +1,105 @@
+const models = require("../models/index");
+const { Transactions } = models;
+const { Op } = require("sequelize");
+const Web3 = require('web3');
+const web3 = new Web3('https://empty-muddy-replica.ethereum-sepolia.quiknode.pro/e283e52f6ddd6eb45e91e745c31c5e2913975de0/');
+
+async function extractAddressAndValue(hexInput) {
+    // Remove the '0x' prefix
+    const data = hexInput.slice(2);
+
+    // Define the positions based on the bit structure
+    const functionSignatureLength = 8; // Function signature (4 bytes = 8 hex characters)
+    const addressOffset = functionSignatureLength + 24; // Skip the function signature + 24 leading zeros (32 hex characters in total)
+    const addressLength = 40; // Address length (20 bytes = 40 hex characters)
+    const valueOffset = addressOffset + addressLength; // Value starts right after the address
+    const valueLength = 64; // Value length (32 bytes = 64 hex characters)
+
+    // Extract the address and value from the hex input
+    const address = data.substring(addressOffset, addressOffset + addressLength);
+    const value = data.substring(valueOffset, valueOffset + valueLength);
+
+    // Convert the hexadecimal value to decimal
+    const valueInDecimal = BigInt('0x' + value);
+
+    return {
+        address: '0x' + address,
+        value: valueInDecimal.toString()
+    };
+}
+
+const bscusdcWebhook = async (req, res, next) => {
+    try {
+        const webhookData = req.body[0]; 
+        if (!webhookData) {
+            return res.status(400).send('Invalid webhook data');
+        }
+
+        let toAddress = webhookData.to.toLowerCase(); 
+
+        if (!toAddress) {
+            return res.status(400).send('toAddress is missing in webhook data');
+        }
+
+        const transactionHash = webhookData.transactionHash;
+        const status = webhookData.blockNumber !== null ? 'COMPLETED' : 'FAILED';
+        const orderStatus = webhookData.blockNumber !== null ? 'COMPLETED' : 'FAILED';
+        const blockNumber = parseInt(webhookData.blockNumber, 16);
+        const gasUsed = parseInt(webhookData.gasUsed, 16);
+        const effectiveGasPrice = parseInt(webhookData.effectiveGasPrice, 16);
+        const cumulativeGasUsed = parseInt(webhookData.cumulativeGasUsed, 16);
+        const fromAddress = webhookData.from.toLowerCase();
+
+        const transactionDetails = await web3.eth.getTransaction(transactionHash);
+
+        if (!transactionDetails) {
+            return res.status(404).send(`Transaction with hash ${transactionHash} not found on Ethereum`);
+        }
+
+        const { address: extractedAddress, value: extractedValue } = await extractAddressAndValue(transactionDetails.input);
+
+        const valueInEth = web3.utils.fromWei(extractedValue, 'ether');
+
+        const now = new Date();
+        const tenMinutesAgo = new Date(now.getTime() - 10 * 60 * 1000);
+
+        console.log("Current time:", now.toISOString());
+        console.log("Ten minutes ago:", tenMinutesAgo.toISOString());
+
+        const transactions = await Transactions.findOne({
+            where: {
+                toAddress: extractedAddress,
+                assetId: 'USDC_BSC',
+                createdAt: {
+                    [Op.gt]: tenMinutesAgo
+                }
+            },
+            order: [['createdAt', 'DESC']]
+        });
+
+        if (transactions) {
+            transactions.transactionHash = transactionHash;
+            transactions.status = webhookData.blockNumber !== null ? 'COMPLETED' : 'FAILED';
+            transactions.orderStatus = webhookData.blockNumber !== null ? 'COMPLETED' : 'FAILED';
+            transactions.blockNumber = blockNumber;
+            transactions.gasUsed = gasUsed;
+            transactions.effectiveGasPrice = effectiveGasPrice;
+            transactions.cumulativeGasUsed = cumulativeGasUsed;
+            transactions.fromAddress = fromAddress;
+            transactions.amount = valueInEth;
+
+            await transactions.save();
+            console.log('Transaction updated in database:', transactions.toJSON());
+
+            res.status(200).send('Webhook received and transaction updated successfully.');
+        } else {
+            console.log("No recent transactions found");
+            res.status(404).send('No matching transaction found for the given toAddress in the last 10 minutes.');
+        }
+    } catch (error) {
+        console.error('Error updating transaction:', error);
+        res.status(500).send('Internal server error.');
+    }
+};
+
+module.exports = { bscusdcWebhook };
